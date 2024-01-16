20a1098c0d411cbc9f576020d8ac288b27d75fb5
max <bruno.m.voss@gmail.com>
2024-01-16 13:26:10 +0100

    added dependencies

---
+++ panda_collisions/panda_collisions.py
@@ -0,0 +1,413 @@
+#for collisions
+from panda3d.core import CollisionTraverser
+from panda3d.core import CollisionNode
+from panda3d.core import CollisionHandlerQueue
+from panda3d.core import CollisionHandlerPusher
+from panda3d.core import CollisionRay
+from panda3d.core import CollisionSphere
+from panda3d.core import BitMask32
+
+from panda3d.core import NodePath
+
+from panda_object_create import panda_object_create_load
+
+# Ok, crash course. This topic is a bit complicated, and may take multiple tries to get.
+
+# highly recommend reading the manual if you're reading comments
+# https://docs.panda3d.org/1.10/python/programming/collision-detection/index
+# yes, do it again. Mind the version I'm linking here.
+
+# You want a collision between two objects.
+
+# fundamentally, the process goes like this:
+    
+    # ObjectA.setup()
+    # ObjectB.setup()
+    # collisiontraverser.traverse()
+    # your_collision = collisionhandlerqueue.getentries()
+    
+# Done.
+
+# Now for the details. E.g. "how to set up mouse click.
+
+# step 1
+# setup_mouse_click creates our "ObjectA" the ray that is being cast
+# from the camera, into the scene.
+
+# step 2
+# create_collision_node creates our "ObjectB" a sphere with radius 0.3
+
+# step 3
+# actually check for the collision with mouse_ray_check
+
+
+# Bonus step: Bitmasks 
+
+# pandas collision system is pretty clever.
+# some parts you WANT to check for collision, like [ray -> enemy] or [ray -> object]
+# and some parts you DONT want to check for collision, like [ray -> grass in front of enemy]
+
+# so panda has bit masks.
+# if two bitmasks OVERLAP, a collision CAN happen
+# if they don't, a collision CANT happen
+
+# e.g.  
+# "mouseray":"from":BitMask32(0b0111)
+# or 111
+# "item":"into":BitMask32(0b0100)
+# or 100
+
+# because the first bit overlaps, the mouse ray can collide with the item!
+
+# compared to:
+# "waypoint": "from":BitMask32(0b0000),
+# "NPC": "into":BitMask32(0b0011)},
+
+# because 000 and 011 DONT overlap, a collision can NEVER happen
+
+# ---
+
+# but because 
+# 111 from the mouse ray and "waypoint":"into":BitMask32(0b0100)
+# AND
+# 111 from the mouse ray and "NPC":"into":BitMask32(0b0011)},
+# BOTH overlap, but in different places,
+# the mouse ray will collide with both and you can click on both.
+
+class CollisionWrapper:
+    def __init__(self,client=True):        
+        self.node_root = NodePath("node_root")
+        self.collision_objects = {}
+        self.collision_bins = {}
+        
+        # see the bitmask step at the beginning for what this means.
+        self.bitmasks={"mouseray":
+            {"from":BitMask32(0b0111),"into":BitMask32(0b0000)},
+                        "terrain":
+            {"from":BitMask32(0b0000),"into":BitMask32(0b0100)},
+                        "waypoint": 
+            {"from":BitMask32(0b0000),"into":BitMask32(0b0100)},
+                        "item":
+            {"from":BitMask32(0b0000),"into":BitMask32(0b0100)},
+                        "players":
+            {"from":BitMask32(0b0001),"into":BitMask32(0b0011)},
+                        "NPC":
+            {"from":BitMask32(0b0001),"into":BitMask32(0b0011)},
+                        "wall":
+            {"from":BitMask32(0b0001),"into":BitMask32(0b0011)},
+            }
+        
+        
+        self.cTrav = CollisionTraverser()
+        self.client_mouse_hover = client
+        
+        if client:
+            self.setup_mouse_ray()
+        pusher = False
+        if pusher:
+        #alternative collisions handlers?
+            self.CH_world = CollisionHandlerPusher() #which means this should be filled
+        else:
+            self.CH_world = CollisionHandlerQueue()
+    
+    def create_new_bin(self,name):
+        P=NodePath(name)
+        P.reparentTo(self.node_root)
+        self.collision_bins[name]=P
+    
+    def update(self,input_d):
+        """
+        The input has to be structured as
+        
+        this will require a dictionary as input
+        
+        {"create":
+            {ob_id:"tag_name",
+            ...},
+         "update":
+            {ob_id:position, # as 3 value tuple or something
+            ...},
+        }
+        """
+        # tags I want... Actors, terrain, UI, Walls/structures?
+        # then check the queue for the collisions I have found.
+        
+        if "create" in input_d:
+            for ob_id in input_d["create"]:
+                
+                tag_name = input_d["create"][ob_id]
+                self.create_collision_node(ob_id,tag_name)
+        
+        if "create complex" in input_d:
+            # ok, doing this, having objects be keys in an input
+            # dict is incredibly annoying, I should not be doing this.
+            for wo_id in input_d["create complex"]:
+                wo = input_d["create complex"][wo_id][0]
+                tagname = input_d["create complex"][wo_id][1]
+                self.create_complex(wo,tagname=tagname)
+        
+        if "update" in input_d:
+            for ob_id in input_d["update"]:
+                if ob_id not in self.collision_objects:
+                    continue
+                
+                data = input_d["update"][ob_id]
+                
+                self.collision_objects[ob_id].setPos(*tuple(data[0]))
+                
+                if data[1] != None:
+                    self.collision_objects[ob_id].setHpr(*tuple(data[1]))
+        
+                
+        if "bin move" in input_d:
+            
+            for ob_id, new_bin in input_d["bin move"]:
+                if new_bin not in self.collision_bins:
+                    self.create_new_bin(new_bin)
+                my_bin=self.collision_bins[new_bin]
+                self.collision_objects[ob_id].reparent_to(my_bin)
+        
+        if "destroy" in input_d:
+            for ob_id in input_d["destroy"]:
+                if ob_id not in self.collision_objects:
+                    continue
+                ob=self.collision_objects[ob_id]
+                ob.removeNode()
+                self.collision_objects.pop(ob_id)
+            input_d["destroy"]=[]
+    
+    def create_complex(self,world_object,tagname="terrain"):
+        """ 
+        this depends on my custom object function, which is kind
+        of optional, so...
+        
+        let's comment that out or move it to a different file?
+        """
+        
+        collision_mask = self.bitmasks[tagname]["into"]
+        my_ob = panda_object_create_load.make_object(self.node_root,world_object.verts,world_object.faces,tag_tuple=(tagname,str(world_object.id)),collision_mask=collision_mask)
+        self.collision_objects[world_object.id] = my_ob
+        my_ob.setPos(*world_object.pos)
+    
+    def clear_all(self):
+        for key in self.collision_objects:
+            ob=self.collision_objects[key]
+            ob.removeNode()
+        
+        self.collision_objects={}
+    
+    def collision_checks(self):
+        #actually look for collisions
+        
+        # node root is the root of a tree
+        # root
+        # - bin
+        #   - object1
+        #   - object2
+        # - bin 2
+        #   - object3
+        #   - object4
+        
+        self.cTrav.traverse(self.node_root)
+        collision_d = self.get_ids_from_collisions()
+        return collision_d
+        
+        if False:
+                if type(world_ob).__name__=="ContainerProjectile":
+                    
+                    tag_tuple=("projectile",str(world_ob.id))
+                    collision_mask=BitMask32.bit(1)
+                    self.attach_collision_node(engine_ob,tag_tuple)
+         
+    def create_collision_node(self,ob_id,tag_name,radius=0.3):
+        """
+        ob_id and tagname are arbitray, but should be basic types that can be used as
+        dictionary keys.
+        
+        step 2 for the crash course
+        """
+        
+        # same pattern as the mouse ray
+        # define node type and shape
+        col_Node = CollisionNode("general_col_node")
+        col_body = CollisionSphere((0,0,0),radius)
+        col_Node.addSolid(col_body)
+        
+        # track the object.
+        col_NodeNP = NodePath(col_Node)
+        col_NodeNP.reparentTo(self.node_root)
+        
+        # tag is being used to recover the object
+        col_NodeNP.set_tag(tag_name,str(ob_id))
+        
+        # add it to another datastructure to track it via id.
+        self.collision_objects[ob_id] = col_NodeNP
+        
+        # set mask
+        # masks handle WHICH collisions are being looked for
+        # see __init__
+        col_Node.setFromCollideMask(self.bitmasks[tag_name]["from"])
+        col_Node.setIntoCollideMask(self.bitmasks[tag_name]["into"])
+        
+        #set it up so it's being tracked.
+        self.cTrav.addCollider(col_NodeNP,self.CH_world)
+        
+    def fetch_objects_from_collision(self,collisionargs):
+        #I should unpack these.
+        
+        fromn = collisionargs[0].from_node
+        inton = collisionargs[0].into_node
+                
+        collision_normal=collisionargs[0].getSurfaceNormal(self.node_root)
+        
+        # what is this?
+        prevt = collisionargs[0]
+        
+        from_tag, from_id = get_info_from_col_node(fromn)
+        into_tag, into_id = get_info_from_col_node(inton)
+        
+        if from_id == into_id:
+            from_id = None
+            into_id = None
+                
+        output_d={"from tag":from_tag,
+                "from id":from_id,
+                "into tag":into_tag,
+                "into id":into_id,
+                "collision normal":collision_normal}
+        
+        return output_d
+    
+    def setup_mouse_ray(self):
+        """
+        sets up the mouse ray?
+        
+        step 1 for the crash course
+        """
+        
+        # Make a collision node for our picker ray
+        self.pickerNode = CollisionNode('mouseRay')
+        self.pickerRay = CollisionRay() 
+        self.pickerNode.addSolid(self.pickerRay)
+        
+        # sets up WHICH collisions are being looked for, 
+        # see "bitmasks" in __init__
+        self.pickerNode.setFromCollideMask(self.bitmasks["mouseray"]["from"])
+        self.pickerNode.setIntoCollideMask(self.bitmasks["mouseray"]["into"])
+        
+        # this adds the ray to the "list" (not a list, but bear with me)
+        # of objects that are being checked for collisions called "node_root"
+        # "node_root" is the "input list"
+        self.pickerNP = NodePath(self.pickerNode)
+        self.pickerNP = self.node_root.attachNewNode(self.pickerNode) # not sure if I need this line.
+        self.pickerNP.reparentTo(self.node_root)
+        
+        
+        # this is the actual collision checker
+        self.MouseRayTraverser = CollisionTraverser()
+        
+        # this is the output list for lack of a better term.
+        self.CHQ_mouse = CollisionHandlerQueue()
+        
+
+        # This means, "if there is a collision with my mouse ray, put it into the CHQ output list"
+        self.MouseRayTraverser.addCollider(self.pickerNP, self.CHQ_mouse)
+                
+        
+    def mouse_ray_check(self,mouse_data):
+        """
+        mouse_data will be 'lensobject, screen_x, screen_y'
+        
+        passing around the lens object isn't ideal, but since this
+        will only happen client side it should be fine.
+        
+        step 3 for the crash course
+        """
+        
+        # Set the position of the ray based on the mouse position
+        (Lens, X, Y), camera_position,camera_hpr = mouse_data
+        self.pickerNP.setPos(camera_position)
+        self.pickerNP.setHpr(camera_hpr)
+        self.pickerRay.setFromLens(Lens, X, Y)
+        
+        # self.node_root is your input list where all your objects
+        # are parented to.
+        self.MouseRayTraverser.traverse(self.node_root)
+        
+        # this is your output list
+        self.CHQ_mouse.sortEntries()
+        n_entries = self.CHQ_mouse.getNumEntries() 
+        
+        
+        # this whole last part answers the question: 
+        # "but WHICH object has my mouse ray collided with?"
+        entry_index = 0
+        tag_names = ["waypoint","terrain","object","NPC","wall","item"]
+        
+        mouse_collision_tags = {}
+        for tag in tag_names:
+            mouse_collision_tags[tag]=[]
+            
+        mouse_3d = None
+        
+        # this is a bit awkward...
+        while entry_index < n_entries and n_entries > 0:
+            
+            # this is how we get the entry and collision object from the underlying C/C++ part of panda
+            collision_entry = self.CHQ_mouse.getEntry(entry_index)
+            collision_node = collision_entry.getIntoNode()
+            
+            # this gets the 3d coordinates of the collision, relative to the root node
+            mouse_3d = collision_entry.getSurfacePoint(self.node_root)
+            
+            # tags are tuples that can be associated with objects
+            # as a "name", "data" pair
+            # so I'm using the "tag name" to get the id of the object
+            for x in tag_names:
+                ob_id = collision_node.getTag(x)
+                if ob_id == '':
+                    continue
+                if ob_id not in mouse_collision_tags[x]:
+                    mouse_collision_tags[x].append(ob_id)
+            
+            # loop around.
+            entry_index += 1
+        
+        # clean up the dictionary.
+        for tag in tag_names:
+            if len(mouse_collision_tags[tag])==0:
+                mouse_collision_tags.pop(tag)
+        
+        # return the last 3d collision point
+        # and the dict of all objects, grouped by tag, that the mouse ray has collided with.
+        return mouse_3d, mouse_collision_tags
+    
+    def get_ids_from_collisions(self):
+        """
+        just detecting and returning ids
+        """
+        
+        collisions = {}
+        entry_check = {}
+        entries = self.CH_world.getEntries()
+        self.CH_world.clearEntries()
+        
+        for entry in entries:
+            collision_dict = self.fetch_objects_from_collision([entry])
+            
+            # checking for none, because they could be deleted?
+            if collision_dict["from id"]!=None and collision_dict["into id"]!=None:
+                if collision_dict["from id"] not in collisions:
+                    collisions[collision_dict["from id"]]={collision_dict["into id"]:collision_dict}
+                
+                #if I am not filtering like this, I have multiple entries.
+                elif collision_dict["into id"] not in collisions[collision_dict["from id"]]:
+                    collisions[collision_dict["from id"]][collision_dict["into id"]]=collision_dict
+                    
+        return collisions
+        
+def get_info_from_col_node(node):
+    tup = node.getTagKeys()
+    tagname = tup[0]
+    from_id = node.get_tag(tagname)
+    return tagname, from_id
---
+++ vector/vector.py
@@ -0,0 +1,439 @@
+
+import math
+"""
+This module assumes you know your way around linear algebra and python.
+It will _not: warn you that something you're doing might not be what
+you want to do. It'll rather do it and fail gracefull.
+
+"""
+
+
+def curl():
+    raise NotImplemented
+    """
+    Takes a vectorfield and the base symbols to be differentiated with.
+    If no base is provided, sympy symbols x,y,z are assumed.
+    """
+    
+def divergence():
+    """returns the sum of the components of the gradient vector"""
+    raise NotImplemented
+        
+
+def gradient():
+    """assumes all free symbols in the expression to be relevant variables.
+    e.g. 
+    (1,1,1) for f(x,y,z)=x+y+z
+    (yz,xz,xy) for f(x,y,z)=x*y*z
+    """
+    raise NotImplemented
+    
+class Matrix:
+    """ 
+    3x3 matrix, takes vectors as input    
+    """
+    def __init__(self,v1,v2,v3):
+        self.v1=v1
+        self.v2=v2
+        self.v3=v3
+        
+    def __getitem__(self,ind):
+        if ind==0:
+            return self.v1
+        elif ind==1:
+            return self.v2
+        elif ind==2:
+            return self.v3
+        else:
+            raise ValueError
+    
+    def __repr__(self):
+        s="\n"
+        c1=0
+        c2=0
+        m=3
+        while c1 < m:
+            while c2 < m:
+                s+=str(self[c1][c2])+" "
+                c2+=1
+            s+="\n"
+            c2=0
+            c1+=1
+        return s
+    
+    def __mul__(self,other):
+        if isinstance(other,Matrix):
+            new_vs=[]
+            for v in other:
+                rv=self*v
+                new_vs.append(rv)
+            M=Matrix(*new_vs)
+            return M
+        if isinstance(other,Vector):
+            i=0
+            j=0
+            new_values=[]
+            while i < 3:
+                s=0
+                while j < 3:
+                    #really? not j i, but i j and I missed this?
+                    val1=self[j][i]
+                    val2=other[j]
+                    r=val1*val2
+                    s+=r
+                    j+=1
+                
+                new_values.append(s)
+                
+                j=0
+                i+=1
+                
+            V=Vector(*new_values)
+            V=round(V,15)
+            return V
+
+
+    def from_Rotation(self,target,z):
+        """ takes two vectors, rotation axis is calculated as cross product
+        then rotation is applied as acos of the dot product"""
+        
+        rot_axis=target.cross(z)
+        angle=math.acos(target.dot(z))
+        M=self.Rotation(angle,None,rot_axis)
+        
+        return M
+        
+def RotationsMatrixFromVector(rot_vector):
+    """not entirely confident this is correct"""
+    
+    rotations=[]
+    if rot_vector[0]!=0:
+        xrot=RotationMatrix(rot_vector[0],(1,0,0))
+        rotations.append(xrot)
+        
+    if rot_vector[1]!=0:
+        yrot=RotationMatrix(rot_vector[1],(0,1,0))
+        rotations.append(yrot)
+        
+    if rot_vector[2]!=0:
+        zrot=RotationMatrix(rot_vector[2],(0,0,1))
+        rotations.append(zrot)
+        
+    return rotations
+
+def RotationMatrix(angle,rot_axis):
+    """angle in radians"""
+    c=math.cos(angle)
+    s=math.sin(angle)
+    
+    x=rot_axis[0]
+    y=rot_axis[1]
+    z=rot_axis[2]
+    
+    t=1-c
+    
+    #oh yeah, straight copied this from wikipedia, just have to get it right.
+    v1=Vector(t*x**2+c  ,t*x*y+z*s, t*x*z-y*s)
+    v2=Vector(t*x*y-z*s ,t*y**2+c,  t*y*z+x*s)
+    v3=Vector(t*x*z+y*s ,t*y*z-x*s, t*z**2+c)
+    
+    M=Matrix(round(v1,6),round(v2,6),round(v3,6))
+    return M
+
+class Vector:
+    
+    def __init__(self,x,y,z):
+        """
+        Works basically like a list or a tuple, except it supports
+        common vector funtions.
+        """
+        
+        #duck type check
+        1+x,1+y,1+z
+        
+        self.x = x 
+        self.y = y 
+        self.z = z 
+    
+    
+    def __lt__(self,other):
+        if type(other)!=Vector:
+            raise TypeError
+        return self.magnitude()<other.magnitude()
+        
+    def __gt__(self,other):
+        if type(other)!=Vector:
+            raise TypeError
+        return self.magnitude()>other.magnitude()
+    
+    def __len__(self):
+        return 3
+    
+    def __getitem__(self,ind):
+        if ind==0:
+            return self.x
+        elif ind==1:
+            return self.y
+        elif ind==2:
+            return self.z
+        else:
+            raise ValueError
+    
+    def __eq__(self,other):
+        
+        if type(other) in [tuple,list]:
+            #this should... take care of type problems.
+            pass
+        elif type(other)!=type(self):
+            return False
+        
+        if len(self)!=len(other):
+            return False
+        c=0
+        l=len(self)
+        while c < l:
+            if self[c]!=other[c]:
+                return False
+            c+=1
+       
+        return True
+    
+    def angle_to_other(self,other):
+        """the angle between two vectors
+        
+        in radians
+        
+        """
+        if type(other)!=type(self):
+            raise TypeError("inputs aren't both vectors.")
+        
+
+        v1=self
+        v2=other
+        
+        m1=round(v1.magnitude(),3)
+        m2=round(v2.magnitude(),3)
+        
+        try:
+            v1=v1.copy()
+            v2=v2.copy()
+            v1=v1.normalize()
+            v2=v2.normalize()
+        except:
+            print("vectors must be normalized, no methods give to support that")
+            
+        ang=math.acos((v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2]))
+        return ang
+    
+    def to_string(self):
+        s="("+",".join([str(self.x),str(self.y),str(self.z)])+")"
+        return s
+    
+    @classmethod
+    def from_string(self,s):
+        s=s.strip("(")
+        s=s.strip(")")
+        s=s.split(",")
+        tup=[float(x) for x in s]
+        return Vector(*tup)
+    
+    def subs(self,d=None):
+        raise NotImplemented
+        #n_exprs=[]
+        #for expr in self:
+            #try:
+                #n_expr=expr.subs(d)
+            ##int and float don't support subs, they would throw attribute errors
+            #except AttributeError:
+                #n_expr=expr
+            #n_exprs.append(n_expr)
+        #return Vector(*n_exprs)
+    def copy(self):
+        return Vector(self.x,self.y,self.z)
+        
+    def cross(self,other):
+        new_x=self.y*other.z-self.z*other.y
+        new_y=self.z*other.x-self.x*other.z
+        new_z=self.x*other.y-self.y*other.x
+        return Vector(new_x,new_y,new_z)
+        
+    def __repr__(self):
+        s=(self.x,self.y,self.z)
+        s=str(s)
+        return s
+    
+            
+    def __neg__(self):
+        return self.__mul__(-1)
+    
+    def __iter__(self):
+        c=0
+        while c <3:
+            yield self[c]
+            c+=1
+    
+    def __rtruediv__(self,quotient):
+        raise TypeError("You can't devide through a vector") 
+    
+    def __ne__(self,other):
+        return not self==other
+    
+    def __truediv__(self,quotient):
+        l=[i/quotient for i in self]        
+        return Vector(*l)
+    
+    def __round__(self,index):
+        """round each entry"""
+        return Vector(round(self[0],index),round(self[1],index),round(self[2],index))
+        
+    
+    def __rmul__(self,other):
+        return self*other
+    
+    def __mul__(self,other):
+        """intended for multiplication of the vector with a scalar"""
+        if isinstance(other,Vector):
+            raise TypeError("use dot product instead")
+        
+        new_vals=[]
+        for val in self:
+            new_vals.append(val*other)
+            
+        return Vector(*new_vals)
+    
+        
+    def __sub__(self,v2):
+        r=Vector(self[0]-v2[0],self[1]-v2[1],self[2]-v2[2])
+        r=round(r,10)
+        return r
+        
+    def __add__(self,v2):
+        v=Vector(self[0]+v2[0],self[1]+v2[1],self[2]+v2[2])
+        v=round(v,10)
+        return v
+    
+    def dot(self,v2):
+        v1=self
+        return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2]
+    
+    def dyadic_polynom(self,v2):
+        """
+        as shown on
+        https://en.wikipedia.org/wiki/Dyadic_tensor
+        
+        returns a polynom
+        """
+        s=0
+        v1=self
+        for val1 in v1:
+            for val2 in v2:
+                s+=val1*val2
+        return s
+        
+    def outer_tensor_product(self,v2):
+        """
+        tensor product as shown on 
+        https://en.wikipedia.org/wiki/Dyadic_tensor
+        the function assumes the vectors are already in the appropriate base.
+        
+        returns a matrix
+        """
+        
+        vectors=[]
+        
+        for val2 in v2:
+            vs=[]
+            for val1 in v1:
+                vs.append(val1*val2)
+                s+=val1*val2
+            v=Vector(*vs)
+            vectors.append(v)
+        m=Matrix(*vectors)
+        return m
+    
+    def set_magnitude(self,l):
+        #ok so the issue is that magnitude is a derived property,
+        #so actually I'm shortening the vector so the result will
+        #be correct.
+        
+        v=self.copy()
+        v=v*(l/self.magnitude())
+        self.x=v.x
+        self.y=v.y
+        self.z=v.z
+        
+    def magnitude(self):
+        v=self
+        l=(v[0]**2+v[1]**2+v[2]**2)**0.5
+        return l
+
+    def normalize(self):
+        v=self
+        l=self.magnitude()
+        if l == 0:
+            return Vector(0,0,0)
+        return Vector(v[0]/l,v[1]/l,v[2]/l)
+
+def angle_v1v2(v1,v2):
+    """the angle between two vectors, in radians
+    
+    """
+    v1=v1.normalize()
+    v2=v2.normalize()
+    val=(v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2])
+    val=min(val,1)
+    
+        
+    ang=math.acos(val)
+    return ang
+    
+def get_radians_from_vector(x,y):
+    angle=0
+    if x>0:
+        angle=math.atan(y/x)
+    if x==0:
+        angle=math.atan(y/0.001)
+    if x<0:
+        angle=math.atan(y/x)+math.pi
+    if x>0 and y<=0:
+        angle=math.atan(y/x)+math.pi*2
+    if x==0 and y<=0:
+        angle=math.atan(y/0.001)+math.pi*2
+    return angle
+
+def vector_interpolation_step(start,goal,step_size=1.5):
+    goal=goal.copy()
+    goal=goal.normalize()
+    start=start.copy()
+    start=start.normalize()
+    
+    rads1=get_radians_from_vector(start[0],start[1])
+    rads2=get_radians_from_vector(goal[0],goal[1])
+    
+    #get difference between the vectors
+    rads_diff=rads2-rads1
+    
+    #make sure to interpolate the shorter path
+    if abs(rads_diff)>math.pi:
+        sign=(rads_diff/abs(rads_diff))
+        if sign>0:
+            rads_diff=(math.pi*2)-abs(rads_diff)
+            rads_diff=-rads_diff
+        else:
+            rads_diff=(math.pi*2)-abs(rads_diff)
+            
+    #if it's bigger than step, use the step
+    #also go the right way.
+    if abs(rads_diff)>step_size:
+        if rads_diff >0:
+            nrads_diff=step_size
+        else:
+            nrads_diff=-step_size
+    else:
+        nrads_diff=rads_diff
+    
+    #get the result.
+    M=RotationMatrix(nrads_diff,Vector(0,0,1))
+    result_vec=M*start
+        
+    return result_vec, rads_diff
